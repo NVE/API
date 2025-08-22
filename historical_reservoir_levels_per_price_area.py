252b82a8a2a5a570c25363b0f859d63f74fa5442
carlandreasveie <106742101+carlandreasveie@users.noreply.github.com>
2025-08-22 16:35:39 +0200

    Create historical_reservoir_levels_per_price_area.py

---
+++ historical_reservoir_levels_per_price_area.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd
+
+
+def historical_reservoir_levels_per_price_area():
+    # API endpoint
+    url = "https://biapi.nve.no/magasinstatistikk/api/Magasinstatistikk/HentOffentligData"
+
+    # Send a GET request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()  # Parse JSON response
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+    df = pd.DataFrame(data)
+
+    df=df[df["omrType"]=="EL"].copy()
+
+    del df["omrType"], df["neste_Publiseringsdato"]
+
+    df=df.rename(columns={"dato_Id":"date","omrnr":"price_area","iso_aar":"iso_year","iso_uke":"iso_week","fyllingsgrad":"reservoir_level", 
+                          "kapasitet_TWh":"capacity_twh","fylling_TWh":"reservoir_level_twh","fyllingsgrad_forrige_uke":"reservoir_level_last_week",
+                          "endring_fyllingsgrad":"reservoir_level_change"})
+
+    df["price_area"]="NO"+df["price_area"].astype(str)
+    df=df.sort_values(by=["price_area","date"], ascending=True).reset_index(drop=True)
+
+    df.to_excel("historical_reservoir_levels_per_price_area.xlsx")
+
+if __name__ == '__main__':
+    historical_reservoir_levels_per_price_area()
